6b4a31e5271a569a08fb92803cbad8309df73495
Nikolai Shcherbin <hello@wzm.me>
2026-03-30 01:00:53 +0300

    More view extensions

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -3,15 +3,21 @@
 return [
 	'plugin' => [
 		'name' => 'My Plugin',
-		'version' => '6.0',
+		'version' => '6.1',
 		'activate_on_install' => true,
 	],
 	
 	'bootstrap' => \MyPlugin\Bootstrap::class, // for more options
 	
 	'view_extensions' => [
+		'admin.css' => [
+			'my_plugin/admin.css' => [],
+		],
 		'elgg.css' => [
-			'my_plugin/style.css' => [],
+			'my_plugin/elgg.css' => [],
+		],
+		'walled_garden.css' => [
+			'my_plugin/walled_garden.css' => [],
 		],
 	],
 ];
